ea8fb00e010af99ea59e6e8888b280757e27ff60
Jacob Kapfer <45276998+jkapfer@users.noreply.github.com>
2021-02-17 02:57:49 -0600

    Added Splunk-Deployer.sh

---
+++ Splunk-Deployer.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+#This is a basic script for deploying Splunk Forwarders on Linux machines and values will
+#have to be input to match local setup. This requires that you have the forwarder downloaded
+#and this script in the same directory.
+#####################################################
+#set your variables for the script or it wont work!!!
+ip=x.x.x.x
+port=x
+#####################################################
+#Forwarder deployment section
+
+#adding forward server based on IP and port number
+echo "adding forward server..."
+./splunk add forward-server <$ip>:<$port> &
+#adding basic monitor, more can be added if necesarry
+echo "adding monitor(s)..."
+./splunk add monitor /var/log &
+#restart to apply settings
+echo "restarting"
+./splunk restart &
+#####################################################
+#iptables configuration section
+
+#opening ports for forwarder to talk to server
+echo "opening ports to commuicate with server..."
+-A INPUT -i eth0 -p tcp --dport $port -m state --state NEW,ESTABLISHED -j ACCEPT &
+-A OUTPUT -o eth0 -p tcp --sport $port -m state --state NEW,ESTABLISHED -j ACCEPT &
+

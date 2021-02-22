106c43b1f4c3c89d2f6bfc1d5dfddc3b6fbdeb69
Jacob Kapfer <45276998+jkapfer@users.noreply.github.com>
2021-02-21 19:06:14 -0600

    added script for creating self signed certs for apache

---
+++ httpd-secure-setup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+sudo yum -y install mod_ssl
+sudo mkdir /etc/ssl/private
+sudo chmod 700 /etc/ssl/private
+cd /etc/ssl/private
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt
+sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+cat /etc/ssl/certs/dhparam.pem | sudo tee -a /etc/ssl/certs/apache-selfsigned.crt

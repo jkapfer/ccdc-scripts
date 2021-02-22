fddc57e5e690d1a60b845778d2231486c285a24c
Jacob Kapfer <45276998+jkapfer@users.noreply.github.com>
2021-02-21 20:06:49 -0600

    setting up variables for when splunk asks for username and password

--- Splunk-Deployer.sh
+++ Splunk-Deployer.sh
@@ -6,15 +6,17 @@
 #set your variables for the script or it wont work!!!
 ip=x.x.x.x
 port=x
+username=x
+password=x
 #####################################################
 #Forwarder deployment section
 
 #adding forward server based on IP and port number
 echo "adding forward server..."
-./splunk add forward-server <$ip>:<$port> &
+./splunk add forward-server <$ip>:<$port>
 #adding basic monitor, more can be added if necesarry
 echo "adding monitor(s)..."
-./splunk add monitor /var/log &
+./splunk add monitor /var/log
 #restart to apply settings
 echo "restarting"
 ./splunk restart &
@@ -25,4 +27,3 @@ echo "restarting"
 echo "opening ports to commuicate with server..."
 -A INPUT -i eth0 -p tcp --dport $port -m state --state NEW,ESTABLISHED -j ACCEPT &
 -A OUTPUT -o eth0 -p tcp --sport $port -m state --state NEW,ESTABLISHED -j ACCEPT &
-

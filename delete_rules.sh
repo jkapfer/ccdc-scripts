7eb0ff8f00fdaab27bcf2a881327ff465ef602cc
Jesse Reed <reedjesselee@gmail.com>
2021-03-05 16:48:23 -0600

    Update delete_rules.sh

--- delete_rules.sh
+++ delete_rules.sh
@@ -1,5 +1,11 @@
 #This is for if you need to delete a rule from all three zones
 
+#Check that user is root
+if [ "$EUID" -ne 0 ]
+  then echo "RUN ONLY WITH SUDO"
+  exit
+fi
+
 #Specify line
 echo Input the line number of the rule. If you just used the block ip script or close iptable script, the line number is 1.
 read line

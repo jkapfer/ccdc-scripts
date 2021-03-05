4dac280f3b8b4a5892ea4413e6736f1148902e24
Jesse Reed <reedjesselee@gmail.com>
2021-03-05 16:49:52 -0600

    Update block_ip.sh

--- block_ip.sh
+++ block_ip.sh
@@ -1,5 +1,11 @@
 #Use this script to quickly block an IP address via iptables
 
+#Check that user is root
+if [ "$EUID" -ne 0 ]
+  then echo "RUN ONLY WITH SUDO"
+  exit
+fi
+
 #Specify the IP you want to block
 echo Input IP you want to block: 
 read ip

e4d9e22d646ea8259eeb6839387b24fb631c1368
Jesse Reed <reedjesselee@gmail.com>
2021-03-05 16:47:58 -0600

    Update close-iptable.sh

--- close-iptable.sh
+++ close-iptable.sh
@@ -1,4 +1,11 @@
 #This script is used to quickly deny all inbound and outbound traffic through your iptable to cut off any persitence
+
+#Check that user is root
+if [ "$EUID" -ne 0 ]
+  then echo "RUN ONLY WITH SUDO"
+  exit
+fi
+
 iptables -I INPUT 1 -j DROP
 iptables -I FORWARD 1 -j DROP
 iptables -I OUTPUT 1 -j DROP

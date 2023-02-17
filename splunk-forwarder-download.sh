e693d94f0559adc17a85725c5086068cf4cd286c
jkapfer <jkapfer33@gmail.com>
2023-02-17 16:42:39 -0600

    adding script because no copy paste uggggggg

---
+++ splunk-forwarder-download.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+wget -O splunkforwarder-9.0.4-de405f4a7979-Linux-x86_64.tgz "https://download.splunk.com/products/universalforwarder/releases/9.0.4/linux/splunkforwarder-9.0.4-de405f4a7979-Linux-x86_64.tgz"
